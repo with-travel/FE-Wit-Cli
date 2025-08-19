21bb39c6fb311b9b18881a6d984bde563f614f17
Juhkang <juhyunk0820@naver.com>
2025-08-19 23:52:09 +0900

    회원가입 로직 navigator 분할 - navigations 재구축

--- src/constants/navigations.ts
+++ src/constants/navigations.ts
@@ -1,10 +1,15 @@
 const authNavigations = {
   AUTH_HOME: 'authHome',
   AUTH_SERVER_LOGIN: 'authServerLogin',
-  AUTH_SERVER_SIGNUP_EMAIL: 'authServerSignupEmail',
-  AUTH_SERVER_SIGNUP_ADDITIONAL_INFO: 'authServerSignupAdditionalInfo',
-  AUTH_SERVER_SIGNUP_PHONE_AUTHORIZATION: 'authServerSignupPhoneAuthorization',
+  AUTH_SIGNUP_FLOW_STACK: 'authSignupFlowStack',
+
   KAKAO: 'Kakao',
 } as const;
 
-export { authNavigations };
+const authFlowNavigations = {
+  AUTH_SERVER_SIGNUP_EMAIL: 'authServerSignupEmail',
+  AUTH_SERVER_SIGNUP_ADDITIONAL_INFO: 'authServerSignupAdditionalInfo',
+  AUTH_SERVER_SIGNUP_PHONE_AUTHORIZATION: 'authServerSignupPhoneAuthorization',
+} as const;
+
+export { authNavigations, authFlowNavigations };
