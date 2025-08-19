ab32e77ad1238a35024b38ff7365e16a5163c3eb
Juhkang <juhyunk0820@naver.com>
2025-08-19 21:17:59 +0900

    babel config 설정

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
+// module.exports = {
+//   presets: ['module:@react-native/babel-preset'],
+// };
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    // 'react-native-reanimated/plugin',
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@': './src',
+        },
+      },
+    ],
+  ],
 };
