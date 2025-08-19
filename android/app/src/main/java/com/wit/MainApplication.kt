06ed60201aa5b3c52add19c261581593baf27ea9
Juhkang <juhyunk0820@naver.com>
2025-08-19 21:19:04 +0900

    라이브러리 android용 의존성 설정

--- android/app/src/main/java/com/wit/MainApplication.kt
+++ android/app/src/main/java/com/wit/MainApplication.kt
@@ -9,6 +9,7 @@ import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
+import com.lugg.RNCConfig.RNCConfigPackage   // ← 추가:react-native-config
 
 class MainApplication : Application(), ReactApplication {
 
@@ -18,6 +19,7 @@ class MainApplication : Application(), ReactApplication {
             PackageList(this).packages.apply {
               // Packages that cannot be autolinked yet can be added manually here, for example:
               // add(MyReactNativePackage())
+              add(RNCConfigPackage())
             }
 
         override fun getJSMainModuleName(): String = "index"
