f276db4061e363b8d38bdf769f0f94c8b68fb5e6
Juhkang <juhyunk0820@naver.com>
2025-08-19 21:17:47 +0900

    eslint 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,16 @@
+// module.exports = {
+//   root: true,
+//   extends: '@react-native',
+// };
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: '@react-native-community',
+  parser: '@babel/eslint-parser',
+  parseroptions: {
+    requireConfigFile: false,
+    babelOptions: {
+      presets: ['module:metro-react-native-babel-preset'],
+    },
+    rules: {},
+  },
 };
