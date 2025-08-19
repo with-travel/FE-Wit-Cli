fe5cec3a2b5a24982bb81236b62f29f4e25fd608
Juhkang <juhyunk0820@naver.com>
2025-08-19 21:20:44 +0900

    진행단계 알림 헤더 바 - 구현예정

---
+++ src/components/auth/ProgressStepBar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+interface ProgressStepBarProps {}
+
+function ProgressStepBar({}: ProgressStepBarProps) {
+  return <View></View>;
+}
+
+const styles = StyleSheet.create({});
+
+export default ProgressStepBar;
