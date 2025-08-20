c2fb452ad261c1cff1a236863d85e094a60ded30
Juhkang <juhyunk0820@naver.com>
2025-08-20 20:07:35 +0900

    핸드폰 인증 화면 - 폰 번호 입력 + 인증 번호 입력 기능 추가

--- src/screens/auth/signup/AuthServerSignupPhoneAuthorizationScreen.tsx
+++ src/screens/auth/signup/AuthServerSignupPhoneAuthorizationScreen.tsx
@@ -1,14 +1,25 @@
 import ProgressStepBar from '@/components/auth/ProgressStepBar';
+import CustomButton from '@/components/common/CustomButton';
+import InputField from '@/components/common/InputField';
 import { useAttachStep } from '@/components/providers/SignupProgressProvider';
-import { authFlowNavigations } from '@/constants/navigations';
-import {
-  AuthStackParamList,
-  SignupFlowStackParamList,
-} from '@/navigations/stack/AuthStackNavigator';
+import { colors } from '@/constants/colors';
+import { authFlowNavigations, authNavigations } from '@/constants/navigations';
+import { SignupFlowStackParamList } from '@/navigations/stack/AuthStackNavigator';
+import { validatePhoneNumber } from '@/utils/validate';
 import { StackScreenProps } from '@react-navigation/stack';
-import React from 'react';
-import { Pressable, StyleSheet, Text, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import React, { useState } from 'react';
+import { Controller, useFormContext } from 'react-hook-form';
+import {
+  LayoutAnimation,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import {
+  SafeAreaView,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
 
 type AuthServerSignupPhoneAuthorizationScreenProps = StackScreenProps<
   SignupFlowStackParamList,
@@ -18,19 +29,109 @@ type AuthServerSignupPhoneAuthorizationScreenProps = StackScreenProps<
 function AuthServerSignupPhoneAuthorizationScreen({
   navigation,
 }: AuthServerSignupPhoneAuthorizationScreenProps) {
-  useAttachStep(3); // ✅
+  const [isSent, setIsSent] = useState(false);
+  const insets = useSafeAreaInsets();
+  useAttachStep(3);
+  const {
+    control,
+    handleSubmit,
+    watch,
+    formState: { isValid },
+  } = useFormContext();
+
+  const onSubmit = () => {
+    navigation.getParent()?.navigate(authNavigations.AUTH_TRAVEL_FORM_STACK);
+  };
+
+  const handlePhoneAuth = () => {
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+    setIsSent(true);
+  };
+
+  const formatPhoneNumber = (text: string) => {
+    const cleaned = ('' + text).replace(/\D/g, '');
+    const match = cleaned.match(/^(010|011|016|017|018|019)(\d{3,4})(\d{4})$/);
+    if (match) {
+      return [match[1], match[2], match[3]].join('-');
+    }
+    return text;
+  };
+
   return (
     <View style={styles.container}>
       <View style={{ paddingHorizontal: 20, paddingTop: 8, marginBottom: 32 }}>
         <ProgressStepBar height={8} />
       </View>
 
-      <Pressable onPress={() => navigation.goBack()}>
-        <Text>이전</Text>
-      </Pressable>
-      <Pressable onPress={() => navigation.push}>
-        <Text>다음3</Text>
-      </Pressable>
+      <View style={styles.infoContainer}>
+        <Controller
+          control={control}
+          name="phoneNumber"
+          render={({
+            field: { onChange, onBlur, value },
+            fieldState: { error },
+          }) => (
+            <View>
+              <InputField
+                label="휴대폰 인증"
+                placeholder="핸드폰 번호를 입력해주세요."
+                onBlur={onBlur}
+                onChangeText={text => onChange(formatPhoneNumber(text))}
+                value={value}
+                keyboardType="phone-pad"
+                maxLength={13}
+                error={error?.message}
+              />
+              {isSent && (
+                <Text style={styles.sentMessage}>
+                  인증 번호가 전송되었습니다.
+                </Text>
+              )}
+            </View>
+          )}
+          rules={{ required: true, validate: validatePhoneNumber }}
+        />
+        {isSent && (
+          <Controller
+            control={control}
+            name="authCode"
+            render={({
+              field: { onChange, onBlur, value },
+              fieldState: { error },
+            }) => (
+              <InputField
+                label="인증번호"
+                placeholder="인증번호 6자리를 입력해주세요."
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+                keyboardType="number-pad"
+                maxLength={6}
+                error={error?.message}
+              />
+            )}
+            rules={{ required: true, minLength: 6 }}
+          />
+        )}
+      </View>
+      <View
+        style={{
+          paddingHorizontal: 20,
+        }}
+      >
+        <CustomButton
+          label={isSent ? '인증번호 확인' : '인증 요청'}
+          onPress={isSent ? () => {} : handlePhoneAuth}
+          style={styles.authButton}
+        />
+      </View>
+      <View style={[styles.buttonCTA, { bottom: insets.bottom + 32 }]}>
+        <CustomButton
+          label="다음"
+          onPress={handleSubmit(onSubmit)}
+          inValid={!isValid}
+        />
+      </View>
     </View>
   );
 }
@@ -38,6 +139,25 @@ function AuthServerSignupPhoneAuthorizationScreen({
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  infoContainer: {
+    gap: 8,
+  },
+  authButton: {
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  buttonCTA: {
+    paddingHorizontal: 20,
+    position: 'absolute',
+    bottom: 32,
+    right: 0,
+    left: 0,
+  },
+  sentMessage: {
+    paddingHorizontal: 20,
+    marginTop: 4,
+    color: colors.PRIMARY_COLOR,
   },
 });
 
