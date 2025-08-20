06bf7ae5acceeaede6a7ad97843069a22dbab1a8
Juhkang <juhyunk0820@naver.com>
2025-08-20 20:09:03 +0900

    AuthStackNav - TravelForm Nav 요소 추가 및 화면 요소 추가

--- src/navigations/stack/AuthStackNavigator.tsx
+++ src/navigations/stack/AuthStackNavigator.tsx
@@ -2,6 +2,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import {
   authFlowNavigations,
   authNavigations,
+  travelFormNavigations,
 } from '../../constants/navigations';
 import AuthHomeScreen from '@/screens/auth/AuthHomeScreen';
 import AuthServerSignupEmailScreen from '@/screens/auth/signup/AuthServerSignupEmailScreen';
@@ -9,13 +10,17 @@ import AuthServerSignupAdditionalInfoScreen from '@/screens/auth/signup/AuthServ
 import AuthServerSignupPhoneAuthorizationScreen from '@/screens/auth/signup/AuthServerSignupPhoneAuthorizationScreen';
 import { colors } from '@/constants/colors';
 import { SignupProgressProvider } from '@/components/providers/SignupProgressProvider';
+import AuthTravelFormCheckScreen from '@/screens/auth/travelform/AuthTravelFormCheckScreen';
+import AuthTravelFormIntroduceScreen from '@/screens/auth/travelform/AuthTravelFormIntroduceScreen';
+import { useForm, FormProvider } from 'react-hook-form';
+import { Pressable } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import Feather from '@react-native-vector-icons/feather';
 
 export type AuthStackParamList = {
   [authNavigations.AUTH_HOME]: undefined;
   [authNavigations.AUTH_SIGNUP_FLOW_STACK]: undefined;
-  // [authNavigations.AUTH_SERVER_SIGNUP_EMAIL]: undefined;
-  // [authNavigations.AUTH_SERVER_SIGNUP_ADDITIONAL_INFO]: undefined;
-  // [authNavigations.AUTH_SERVER_SIGNUP_PHONE_AUTHORIZATION]: undefined;
+  [authNavigations.AUTH_TRAVEL_FORM_STACK]: undefined;
 };
 
 export type SignupFlowStackParamList = {
@@ -24,9 +29,16 @@ export type SignupFlowStackParamList = {
   [authFlowNavigations.AUTH_SERVER_SIGNUP_PHONE_AUTHORIZATION]: undefined;
 };
 
+export type TravelFormStackParamList = {
+  [travelFormNavigations.AUTH_TRAVEL_FORM_CHECK]: undefined;
+  [travelFormNavigations.AUTH_TRAVEL_FORM_INTRODUCE]: undefined;
+};
+
 const Stack = createStackNavigator<AuthStackParamList>();
 
 const SignupStack = createStackNavigator<SignupFlowStackParamList>();
+
+const TravelFormStack = createStackNavigator<TravelFormStackParamList>();
 
 function SignupFlowNavigator() {
   return (
@@ -74,57 +86,100 @@ function SignupFlowNavigator() {
   );
 }
 
-function AuthStackNavigator() {
+function TravelFormNavigator() {
+  const navigation = useNavigation();
   return (
-    <Stack.Navigator
-      screenOptions={{
-        headerShown: false,
-        cardStyle: {
-          backgroundColor: colors.UNCHANGED_WHITE,
-        },
-      }}
-    >
-      <Stack.Screen
-        name={authNavigations.AUTH_HOME}
-        component={AuthHomeScreen}
-        options={{
+    <SignupProgressProvider totalSteps={2}>
+      <TravelFormStack.Navigator
+        screenOptions={{
           headerShown: false,
+          cardStyle: {
+            backgroundColor: colors.UNCHANGED_WHITE,
+          },
         }}
-      />
-      <Stack.Screen
-        name={authNavigations.AUTH_SIGNUP_FLOW_STACK}
-        component={SignupFlowNavigator}
-        options={{
-          headerShown: false,
-        }}
-      />
-      {/* <Stack.Screen
-          name={authNavigations.AUTH_SERVER_SIGNUP_EMAIL}
-          component={AuthServerSignupEmailScreen}
+      >
+        <TravelFormStack.Screen
+          name={travelFormNavigations.AUTH_TRAVEL_FORM_CHECK}
+          component={AuthTravelFormCheckScreen}
           options={{
             headerShown: true,
-            headerTitle: '회원가입',
-            // animation: 'none',
+            animation: 'none',
+            headerTitle: '여행 설문',
+            headerLeft: () => (
+              <Pressable onPress={() => navigation.goBack()}>
+                <Feather
+                  name="chevron-left"
+                  size={24}
+                  color={colors.UNCHANGED_BLACK}
+                />
+              </Pressable>
+            ),
             headerBackButtonDisplayMode: 'minimal',
           }}
         />
+        <TravelFormStack.Screen
+          name={travelFormNavigations.AUTH_TRAVEL_FORM_INTRODUCE}
+          component={AuthTravelFormIntroduceScreen}
+          options={{
+            headerShown: true,
+            headerTitle: '여행 설문',
+            animation: 'none',
+            headerBackButtonDisplayMode: 'minimal',
+          }}
+        />
+      </TravelFormStack.Navigator>
+    </SignupProgressProvider>
+  );
+}
+
+function AuthStackNavigator() {
+  const methods = useForm({
+    mode: 'onChange',
+    defaultValues: {
+      email: '',
+      password: '',
+      passwordConfirm: '',
+      nickname: '',
+      name: '',
+      birth: '',
+      gender: '',
+      phoneNumber: '',
+      authCode: '',
+    },
+  });
+  return (
+    <FormProvider {...methods}>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+          cardStyle: {
+            backgroundColor: colors.UNCHANGED_WHITE,
+          },
+        }}
+      >
         <Stack.Screen
-          name={authNavigations.AUTH_SERVER_SIGNUP_ADDITIONAL_INFO}
-          component={AuthServerSignupAdditionalInfoScreen}
+          name={authNavigations.AUTH_HOME}
+          component={AuthHomeScreen}
           options={{
             headerShown: false,
-            animation: 'none',
           }}
         />
         <Stack.Screen
-          name={authNavigations.AUTH_SERVER_SIGNUP_PHONE_AUTHORIZATION}
-          component={AuthServerSignupPhoneAuthorizationScreen}
+          name={authNavigations.AUTH_SIGNUP_FLOW_STACK}
+          component={SignupFlowNavigator}
           options={{
             headerShown: false,
-            animation: 'none',
           }}
-        /> */}
-    </Stack.Navigator>
+        />
+        <Stack.Screen
+          name={authNavigations.AUTH_TRAVEL_FORM_STACK}
+          component={TravelFormNavigator}
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack.Navigator>
+    </FormProvider>
   );
 }
 
