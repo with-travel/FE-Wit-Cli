35684cb417b478dc8950fc4e441c520d70b442b3
Juhkang <juhyunk0820@naver.com>
2025-08-20 20:07:05 +0900

    phone number validate

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -2,3 +2,8 @@ export const validateEmail = (email: string) => {
   const re = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   return re.test(String(email).toLowerCase());
 };
+
+export const validatePhoneNumber = (phoneNumber: string) => {
+  const re = /^01[016789]-\d{3,4}-\d{4}$/;
+  return re.test(phoneNumber);
+};
