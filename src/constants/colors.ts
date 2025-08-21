471546ec1c1504686dc4b9f3e510c0c45d1bc2b6
Juhkang <juhyunk0820@naver.com>
2025-08-22 00:00:29 +0900

    선형 색상 저장

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -3,17 +3,24 @@ const common = {
   GRAY_100: '#D9D9D9',
   GRAY_200: '#0007234D',
   GRAY_400: '#0007234D',
+  GRAY_500: '#808080',
   RED_100: '#FF7B5D',
   RED_300: '#FFB4B4',
   RED_500: '#FF5F5F',
   BLUE_500: '#0D8AFF',
-  PRIMARY_COLOR: '#359D66',
+  PRIMARY_COLOR: '#4EB15D',
   UNCHANGED_WHITE: '#FFF',
   UNCHANGED_BLACK: '#000',
 };
 
+const Linear = {
+  LINEAR_ORANGE: ['#FF6B26', '#FFAF2F'],
+  LINEAR_BLUE: ['#0098F0', '#0DA5AD'],
+};
+
 const colors = {
   ...common,
+  ...Linear,
 };
 
 export { colors };
