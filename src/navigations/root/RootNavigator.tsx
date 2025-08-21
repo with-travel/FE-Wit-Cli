e48133e3aa56dfe64db08661f31cc8677aa29963
Juhkang <juhyunk0820@naver.com>
2025-08-22 00:01:06 +0900

    auth 값에 따른 분기 설정 필요 - MainTab/Auth

--- src/navigations/root/RootNavigator.tsx
+++ src/navigations/root/RootNavigator.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import Config from 'react-native-config';
 import AuthStackNavigator from '../stack/AuthStackNavigator';
+import MainTabNavigator from '../tab/MainTabNavigator';
 
 function RootNavigator() {
   console.log('Config.SERVER_BASEURL', Config.SERVER_BASEURL);
-  return <AuthStackNavigator />;
+  // return <AuthStackNavigator />;
+  return <MainTabNavigator />;
 }
 
 export default RootNavigator;
