e6b19e97e95ba6b459632600a26bf5fac0589bc6
Juhkang <juhyunk0820@naver.com>
2025-08-23 17:45:43 +0900

    remove unused things

--- src/screens/maintab/HomeScreen.tsx
+++ src/screens/maintab/HomeScreen.tsx
@@ -1,25 +1,10 @@
-// src/screens/(tabs)/HomeScreen.tsx 등 4개 더미
 import { colors } from '@/constants/colors';
 import React from 'react';
 import { View, Text, StyleSheet, Image, Dimensions } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
+
 export default function HomeScreen() {
-  return (
-    <View style={styles.container}>
-      {/* <LinearGradient
-        colors={colors.LINEAR_ORANGE}
-        start={{ x: 0, y: 0 }}
-        end={{ x: 1, y: 0 }}
-        style={styles.header}
-      >
-        <Image
-          source={require('@/assets/images/WitLogoWhite.png')}
-          style={styles.logo}
-        />
-      </LinearGradient> */}
-      <Text>Home</Text>
-    </View>
-  );
+  return <View style={styles.container}></View>;
 }
 
 const styles = StyleSheet.create({
