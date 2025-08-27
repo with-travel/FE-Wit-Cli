003433e95e14e7b973c262e589cba1feb7997e7f
Juhkang <juhyunk0820@naver.com>
2025-08-28 01:48:59 +0900

    eslint설정 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,17 @@
-// module.exports = {
-//   root: true,
-//   extends: '@react-native',
-// };
+// .eslintrc.js
 module.exports = {
   root: true,
-  extends: '@react-native-community',
-  parser: '@babel/eslint-parser',
-  parserOptions: {
-    requireConfigFile: false,
-    babelOptions: {
-      presets: ['module:metro-react-native-babel-preset'],
-    },
-    rules: {},
-  },
+  extends: ['@react-native'],
 };
+// module.exports = {
+//   root: true,
+//   extends: '@react-native-community',
+//   parser: '@babel/eslint-parser',
+//   parserOptions: {
+//     requireConfigFile: false,
+//     babelOptions: {
+//       presets: ['module:metro-react-native-babel-preset'],
+//     },
+//     rules: {},
+//   },
+// };
