6ce3138a352dd69536eb09329f31da1459af0c47
Juhkang <juhyunk0820@naver.com>
2025-08-31 23:18:23 +0900

    queryKey 요소 추가

--- src/constants/keys.ts
+++ src/constants/keys.ts
@@ -1,9 +1,11 @@
 const storageKeys = {
   ACCESS_TOKEN: 'accessToken',
+  REFRESH_TOKEN: 'refreshToken',
 } as const;
 
 const queryKeys = {
   AUTH: 'auth',
+  GET_USER_INFO: 'getUserInfo',
 } as const;
 
 export { storageKeys, queryKeys };
