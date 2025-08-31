f6ad771487f8bef9ca086a9b4c4e42525b324286
Juhkang <juhyunk0820@naver.com>
2025-08-31 23:18:56 +0900

    type 설정:reqeust:auth

--- src/types/request/auth.ts
+++ src/types/request/auth.ts
@@ -1,22 +1,43 @@
-type LoginRequest = {
+type RequestLogin = {
   email: string;
   password: string;
 };
 
-type SignupRequest = {
+type RequestSignup = {
   email: string;
   password: string;
+};
+
+type ExtraInfo = {
+  nickname: string;
+  birthdate: string;
+  gender: 'MALE' | 'FEMALE';
+  introduction: string;
+  email: string;
+  password: string;
+  name: string;
+  phone: string;
+};
+
+type Survey = {
+  energyLevels: string;
+  travelGoals: string;
+  travelPaces: string;
+  commStyles: string;
+  recordTendencies: string;
+  companionStyles: string;
+  spendPatterns: string;
 };
 
 type UserInfoSignupRequest = {
-  extraInfo: {
-    nickname: string;
-    name: string;
-    gender: 'MALE' | 'FEMALE';
-    birthDate: string;
-    introduction: string;
-  };
-  survey: {};
+  extraInfo: ExtraInfo;
+  survey: Survey;
 };
 
-export type { LoginRequest, SignupRequest, UserInfoSignupRequest };
+export type {
+  RequestLogin,
+  RequestSignup,
+  UserInfoSignupRequest,
+  ExtraInfo,
+  Survey,
+};
