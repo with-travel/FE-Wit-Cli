b03690525330ea1b9c112fc56be80dc038bddc14
JuhKang <47581536+juhyunk0820@users.noreply.github.com>
2025-09-01 23:14:01 +0900

    Develop (#67)
    * tanstack query custom 옵션 설정
    
    * useAuth hook 수정: 커스텀 query 적용
    
    * 사용자 로그인에 따른 화면 라우팅

--- src/navigations/root/RootNavigator.tsx
+++ src/navigations/root/RootNavigator.tsx
@@ -3,11 +3,14 @@ import { Text, View } from 'react-native';
 import Config from 'react-native-config';
 import AuthStackNavigator from '../stack/AuthStackNavigator';
 import MainTabNavigator from '../tab/MainTabNavigator';
+import useAuth from '@/hooks/queries/useAuth';
 
 function RootNavigator() {
   console.log('Config.SERVER_BASEURL', Config.SERVER_BASEURL);
+  const { auth } = useAuth();
   // return <AuthStackNavigator />;
-  return <MainTabNavigator />;
+  // return <MainTabNavigator />;
+  return <>{auth.id ? <MainTabNavigator /> : <AuthStackNavigator />}</>;
 }
 
 export default RootNavigator;
