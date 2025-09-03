70999a1447b9295fe6d4540d6d55f621553d4ad7
Juhkang <juhyunk0820@naver.com>
2025-09-03 22:42:24 +0900

    auth 관련 api 수정: 주소 변경 및 param 추가

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -19,19 +19,22 @@ const duplicateCheckNickName = async (nickname: string): Promise<boolean> => {
 };
 
 const duplicateCheckEmail = async (email: string) => {
-  const response = await axiosInstance.get(
-    `/auth/duplicate-check/email?email=${email}`,
-  );
+  const response = await axiosInstance.get(`/api/v1/auth/email-available`, {
+    params: {
+      email,
+    },
+  });
   return response.data;
 };
 
 const postLogin = async (body: RequestLogin): Promise<ResponseLogin> => {
-  const response = await axiosInstance.post('/auth/login', body);
+  const response = await axiosInstance.post('/api/v1/auth/login', body);
   return response.data;
 };
 
 const postSignup = async (body: RequestSignup): Promise<ResponseSignup> => {
-  const response = await axiosInstance.post('/auth/signup', body);
+  const response = await axiosInstance.post('/api/v1/auth/signup', body);
+
   return response.data;
 };
 
